478fc12969bb63c9429b7ce2de3ad36aeed9fcb3
Pedro <pedrolucf@gmail.com>
2025-07-01 21:40:17 -0300

    fix: conig

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,21 @@
-import adapter from '@sveltejs/adapter-static'; // MUDANÇA IMPORTANTE AQUI
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+	preprocess: vitePreprocess(),
 
-    kit: {
-        // MUDANÇA IMPORTANTE AQUI
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: '404.html',
-            precompress: false,
-            strict: true
-        }),
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html'
+		}),
 
-        paths: {
-            // ATENÇÃO: Verifique se o nome do seu repositório está correto aqui!
-            base: process.env.NODE_ENV === 'production' ? '/Portifolio' : '',
-        }
-    }
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/Portifolio' : ''
+		}
+	}
 };
 
 export default config;
