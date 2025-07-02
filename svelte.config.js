3359df07231c268d56eb897d20d56766257caa4c
Pedro <pedrolucf@gmail.com>
2025-07-01 21:20:54 -0300

    fix: Configura o adapter-static para deploy no GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,25 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static'; // MUDANÇA IMPORTANTE AQUI
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        // MUDANÇA IMPORTANTE AQUI
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            strict: true
+        }),
+
+        paths: {
+            // ATENÇÃO: Verifique se o nome do seu repositório está correto aqui!
+            base: process.env.NODE_ENV === 'production' ? '/Portifolio' : '',
+        }
+    }
 };
 
 export default config;
